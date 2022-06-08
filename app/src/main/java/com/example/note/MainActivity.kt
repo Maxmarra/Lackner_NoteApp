0ee53a331f8c20bd2f4293f2990450952c208342
Maxmarra <5919669@gmail.com>
2022-06-08 08:55:41 +0300

    starter

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
             NoteTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
